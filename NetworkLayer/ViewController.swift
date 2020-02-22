dfd6093bc7bfa7a3f441a0d9220bdd2879c057bb
Ramy Nasser <ramy.assr@vodafone.com>
2020-02-22 13:30:37 +0200

    Initial Commit

---
+++ NetworkLayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkLayer
+//
+//  Created by Ramy Nasser on 2/22/20.
+//  Copyright © 2020 Ramy Nasser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
